83b9a7ba3be7fd8dd7034d4e3367bf80626355ac
rajat0913 <113683890+rajat0913@users.noreply.github.com>
2022-10-17 12:54:08 +0530

    Update counting.cpp

--- counting.cpp
+++ counting.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-
 using namespace std;
 
 int getMax(int a[], int n)
@@ -10,39 +9,36 @@ int getMax(int a[], int n)
         if (a[i] > max)
             max = a[i];
     }
-    return max; // maximum element from the array
+    return max;
 }
 
-void countSort(int a[], int n) // function to perform counting sort
+void countSort(int a[], int n) 
 {
     int output[n + 1];
     int max = getMax(a, n);
-    int count[max + 1]; // create count array with size [max+1]
+    int count[max + 1];
 
     for (int i = 0; i <= max; ++i)
     {
-        count[i] = 0; // Initialize count array with all zeros
+        count[i] = 0; 
     }
 
-    for (int i = 0; i < n; i++) // Store the count of each element
+    for (int i = 0; i < n; i++) 
     {
         count[a[i]]++;
     }
 
     for (int i = 1; i <= max; i++)
-        count[i] += count[i - 1]; // find cumulative frequency
-
-    /* This loop will find the index of each element of the original array in count array, and
-     place the elements in output array*/
+        count[i] += count[i - 1];
     for (int i = n - 1; i >= 0; i--)
     {
         output[count[a[i]] - 1] = a[i];
-        count[a[i]]--; // decrease count for same numbers
+        count[a[i]]--;
     }
 
     for (int i = 0; i < n; i++)
     {
-        a[i] = output[i]; // store the sorted elements into main array
+        a[i] = output[i];
     }
 }
 
